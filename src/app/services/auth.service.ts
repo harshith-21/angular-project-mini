c04af761985648072c962bd5824ef5a391d1248d
HARSHITH GANDHE <gandheharshith21@gmail.com>
2022-10-06 17:11:42 +0530

    added header and basic homepage
    header done with logo and logiut buttons...logo button goes to home and and logout button goes to login page
    home page has a line to divide all movies and popular movies

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-// import { Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   login(uname: string, pword: string) {
     if (uname === 'admin' && pword === 'admin') {
@@ -14,5 +14,10 @@ export class AuthService {
     } else {
       return 403;
     }
+  }
+
+
+  logout() {
+    this.router.navigate(['login']);
   }
 }
